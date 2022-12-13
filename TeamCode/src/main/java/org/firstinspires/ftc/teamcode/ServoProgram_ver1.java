580c924fd520079430bbd14e5d87a63f9c0d61be
JohnJDuBois <jdubois@chesteruplandsd.org>
2022-12-13 11:50:28 -0500

    Basic Servo Code from FTC

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoProgram_ver1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoProgram_ver1.java
@@ -47,7 +47,7 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-@TeleOp(name = "Concept: Scan Servo", group = "Concept")
+@TeleOp(name = "ServoProgram_ver1", group = "Concept")
 // @Disabled
 public class ServoProgram_ver1 extends LinearOpMode {
 
